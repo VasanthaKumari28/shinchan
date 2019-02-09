2479e92119794657f12d73fc4ea1dddb2982f7dc
VasanthaKumari28 <47455897+VasanthaKumari28@users.noreply.github.com>
2019-02-09 16:35:16 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a<0)
+    {
+        printf("invalid");
+    }
+    else if(a%2==0)
+    {
+        printf("Even");
+    
+    }
+    else if(a%2!=0)
+    {
+        printf("Odd");
+    }
+     return 0;
+}
